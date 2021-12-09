77dc901f4859d5f9e4a15c7c8d5788ea025c4445
danielbroxy <95831461+danielbroxy@users.noreply.github.com>
2021-12-10 01:33:41 +0300

    droch

---
+++ Base-C/Base-C/droch_s_massivom.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    const int a;
+    cin >> a;
+    int A[a];
+
+}
--- Base-C/Base-C/privetmir.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Privet, mir!";
-    return 0;
-}
